cbaf516f124cb5af06792c6a4506b5f019af9205
futongttmm <futong101@gmail.com>
2019-07-08 22:19:52 +0800

    Add Database First Workflow

---
+++ Vidly/Migrations/201907081407581_PopulateMembershipTypesAndGenre.cs
@@ -0,0 +1,26 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypesAndGenre : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (1, 'Pay As You Go',  0, 0, 0)");
+            Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (2, 'Monthly', 30, 1, 10)");
+            Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (3, 'Seasonly', 90, 3, 15)");
+            Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (4, 'Anually', 300, 12, 20)");
+
+            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Action')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Thriller')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Family')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Romance')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Comedy')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
